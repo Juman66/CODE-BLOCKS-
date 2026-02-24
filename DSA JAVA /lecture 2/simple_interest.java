6bfd94d8a405134e92db3d5ea0569504637a15af
JUMAN DEKA <jumandeka2004@gmail.com>
2026-02-24 13:34:36 +0530

    Delete DSA JAVA /lecture 2 directory

--- DSA JAVA /lecture 2/simple_interest.java
+++
@@ -1,9 +0,0 @@
-public class simple_interest {
-    public static void main(String[] args) {
-        int p =5;
-        int r= 6;
-        int t = 7;
-        int si= (p*r*t)/100;
-        System.out.println(si);
-    }
-}
