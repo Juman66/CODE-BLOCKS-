12fb71c1d196810c9d0ee595921ef1c856d726cc
JUMAN DEKA <jumandeka2004@gmail.com>
2026-02-26 13:10:12 +0530

    Add files via upload

---
+++ DSA JAVA/lecture 14/missing.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n= nums.length;
+        int total = n*(n+1)/2;
+        int arraysum = 0;
+        for(int i = 0; i<nums.length;i++){
+            arraysum=arraysum+nums[i];
+        }
+        int output = total-arraysum;
+        return output;
+    }
+}//leetcode 268
